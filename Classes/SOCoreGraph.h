e07f7837ea1a2eb10e886143251643f2cc3f0a27
Stephan Zehrer <stephan@zehrer.net>
2014-05-19 21:38:29 +0200

    fix some minor issues with the migration to the github project

--- Classes/SOCoreGraph.h
+++
@@ -1,9 +0,0 @@
-//
-//  SOCoreGraph.h
-//  SOCoreGraph
-//
-//  Created by Stephan Zehrer on 15.09.13.
-//  Copyright (c) 2013 Stephan Zehrer. All rights reserved.
-//
-
-
--- Classes/SORelationship.h
+++ Classes/SORelationship.h
@@ -6,7 +6,6 @@
 //  Copyright (c) 2013 Stephan Zehrer. All rights reserved.
 //
 
-#import "SOCoreGraph.h"
 #import "SOPropertyAccessElement.h"
 
 #import "SOListElement.h"
