943bb07bf9b877f322c0283cd1adb5ebd38aaefb
Stephan Zehrer <stephan@zehrer.net>
2014-05-19 21:20:45 +0200

    new library/framework header file

---
+++ Classes/SOGraphDB.h
@@ -0,0 +1,14 @@
+//
+//  SOGraphDB.h
+//  SOGraphDB
+//
+//  Created by Stephan Zehrer on 03.05.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SOGraphContext.h"
+
+#import "SONode.h"
+#import "SORelationship.h"
