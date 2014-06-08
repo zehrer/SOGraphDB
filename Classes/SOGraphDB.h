8ca42773c6b0bdc27f138ed480746b6e7b301aba
Stephan Zehrer <stephan@zehrer.net>
2014-06-08 19:43:37 +0200

    add further public header files

--- Classes/SOGraphDB.h
+++ Classes/SOGraphDB.h
@@ -12,11 +12,33 @@
 //! Project version string for SOGraphDB.
 //FOUNDATION_EXPORT const unsigned char SOGraphDBVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SOGraphDB/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import NSStoreCoder.h<SOGraphDB/PublicHeader.h>
 
 #import <SOGraphDB/SOCoding.h>
 
 #import <SOGraphDB/SOGraphContext.h>
 
+#import <SOGraphDB/SOGraphElement.h>
+
 #import <SOGraphDB/SONode.h>
 #import <SOGraphDB/SORelationship.h>
+#import <SOGraphDB/SOProperty.h>
+#import <SOGraphDB/SOPropertyAccess.h>
+#import <SOGraphDB/SOPropertyAccessElement.h>
+
+#import <SOGraphDB/SOFileStore.h>
+#import <SOGraphDB/SODataStore.h>
+#import <SOGraphDB/SOObjectStore.h>
+#import <SOGraphDB/SOManagedDataStore.h>
+
+#import <SOGraphDB/SOStringData.h>
+#import <SOGraphDB/SOStringStore.h>
+#import <SOGraphDB/SOStringDataStore.h>
+
+#import <SOGraphDB/SOCSVReader.h>
+#import <SOGraphDB/SOTools.h>
+
+#import <SOGraphDB/SOListElement.h>
+
+#import <SOGraphDB/NSURL+SOCore.h>
+#import <SOGraphDB/NSStoreCoder.h>
