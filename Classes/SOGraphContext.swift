607ce53059ec1e519f48186f094d6da004ec5fc4
Stephan Zehrer <stephan@zehrer.net>
2014-06-09 09:30:27 +0200

    initial version

---
+++ Classes/SOGraphContext.swift
@@ -0,0 +1,12 @@
+//
+//  SOGraphContext.swift
+//  SOGraphDB
+//
+//  Created by Stephan Zehrer on 07.06.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+import Foundation
+
+
+
