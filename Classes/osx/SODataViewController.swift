69ee72692f1a81930d06ec9df8b51fdaa8883801
Stephan Zehrer <stephan@zehrer.net>
2014-06-10 07:54:56 +0200

    Initial Version

---
+++ Classes/osx/SODataViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SODataViewController.swift
+//  SOGraphDB
+//
+//  Created by Stephan Zehrer on 09.06.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+import Cocoa
+
+/**
+class SODataViewController : NSTableViewDataSource, NSTableViewDelegate {
+    
+    
+    
+}
+
+*/
