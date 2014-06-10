ad5b2cce46c9ffc7cfe227344c1e39a12e3c13c1
Stephan Zehrer <stephan@zehrer.net>
2014-06-10 08:20:39 +0200

    initial version

---
+++ Demo/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Demo
+//
+//  Created by Stephan Zehrer on 09.06.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
