1be5311e7a9651cb07700c42df5da613a7dc57d6
Stephan Zehrer <stephan@zehrer.net>
2014-06-10 21:04:37 +0200

    Initial version

---
+++ Classes/Property.swift
@@ -0,0 +1,14 @@
+//
+//  Property.swift
+//  SOGraphDB
+//
+//  Created by Stephan Zehrer on 10.06.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+import Foundation
+
+protocol Property {
+    
+    
+}
---
+++ Classes/osx/Relationship.swift
@@ -0,0 +1,15 @@
+//
+//  Relationship.swift
+//  SOGraphDB
+//
+//  Created by Stephan Zehrer on 10.06.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+import Foundation
+
+protocol Relationship {
+    
+    init(startNode : Node)
+
+}
