2223522cf924fb20458a302f81d5fe0851beb1d8
Stephan Zehrer <stephan@zehrer.net>
2014-06-10 08:19:06 +0200

    initial version

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Stephan Zehrer on 09.06.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+import Cocoa
+import SOGraphDB
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
