395d982ec1c114a628307ec4e34f461b88efbf0a
Stephan Zehrer <stephan@zehrer.net>
2014-06-30 19:43:58 +0200

    add initStore and header

--- ObjectStore.swift
+++ ObjectStore.swift
@@ -8,12 +8,39 @@
 
 import Foundation
 
-class ObjectStore<O: PersistentObject, H: DataStoreHeader> : DataStore<O.DataType,H> {
+struct ObjectStoreHeader : DataStoreHeader  {
+
+    var used: Bool = true;
+    
+}
+
+class ObjectStore<O: PersistentObject> : DataStore<ObjectStoreHeader,O.DataType> {
     
     //var objectType : Class!
     
     init(url: NSURL) {
         super.init(url: url)
+    }
+    
+    // subclasses should overide this method
+    // Create a file store element with the ID:0
+    // ID 0 is not allowd to use in the store because
+    override func initStore() {
+        
+        if self.newFile {
+            
+            // store SampleData as ID:0 in the file
+            // ID:0 is a reserved ID and should not be availabled for public access
+            
+            let aObj = O()
+            
+            var header = ObjectStoreHeader(used: false)
+   
+            self.writeHeader(&header)
+            self.writeData(aObj.data, atPos: CUnsignedLongLong(self.fileOffset))
+            
+        }
+        
     }
     
     func registerObject() -> UID {
