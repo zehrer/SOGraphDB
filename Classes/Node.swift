cc54430543613dc97b12d0c962d7489c01e5391b
Stephan Zehrer <stephan@zehrer.net>
2014-08-01 21:47:24 +0200

    initial code

--- Classes/Node.swift
+++ Classes/Node.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+/**
 protocol Node {
     
     // OUT
@@ -40,4 +41,120 @@ protocol Node {
     //func inNodeEnumerator() -> NSEnumerator
     
 }
+*/
 
+struct NODE : Init {
+    var nextPropertyID: UID = 0; // 4
+    var nextOutRelationshipID: UID  = 0;  // 4
+    var nextInRelationshipID: UID  = 0; // 4
+    
+    init() {
+        
+    }
+}
+
+
+
+class Node : GraphElement, Coding {
+
+    var data: NODE = NODE()
+
+    //var uid: UID? = nil
+    //var dirty: Bool = true
+    
+    init() {
+        
+    }
+    
+    //decoding NSData
+    init(data: NODE) {
+        self.data = data
+        
+    }
+    
+    
+    
+    // OUT
+    var outRelationshipCount: Int? {
+    get {
+        return 0;
+    }
+    }
+    
+    var firstOutNode: Node? {
+    get {
+        return nil;
+    }
+    }
+    
+    var lastOutNode: Node? {
+    get {
+        return nil;
+    }
+    }
+    
+    var outRelationships: [Relationship] {
+    get {
+        return [Relationship]()
+    }
+    }
+    
+    func outRelationshipTo(node: Node) -> Relationship? {
+        return nil;
+    }
+    
+    func addOutRelationshipNode(node: Node) -> Relationship? {
+        return nil;
+    }
+    
+    func deleteOutRelationshipNode(node: Node) {
+        
+    }
+    
+    /*
+    func outNodeEnumerator() -> NSEnumerator {
+    return SONodeEnumerator(
+    }
+    */
+    
+    
+    // IN
+    var inRelationshipCount: Int? {
+    get {
+        return 0;
+    }
+    }
+    
+    var firstInNode: Node? {
+    get {
+        return nil;
+    }
+    }
+    
+    var lastInNode: Node? {
+    get {
+        return nil;
+    }
+    }
+    
+    var inRelationships: [Relationship] {
+    get {
+        return [Relationship]()
+    }
+    }
+    
+    func inRelationshipFrom(node: Node) -> Relationship? {
+        return nil;
+    }
+    
+    func addInRelationshipNode(node: Node) -> Relationship? {
+        return nil;
+    }
+    
+    func deleteInRelationshipNode(node: Node) {
+        
+    }
+    
+    //func inNodeEnumerator() -> NSEnumerator
+    
+}
