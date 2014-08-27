74f6d517946510991501955c68bbbd1deff448cd
Stephan Zehrer <stephan@zehrer.net>
2014-08-27 20:21:13 +0200

    updated

--- Tests/DataStoreTests.swift
+++ Tests/DataStoreTests.swift
@@ -11,7 +11,7 @@ import SOGraphDB
 
 struct TestHeader : DataStoreHeader  {
     
-    var used: Bool = true;
+    var used: Bool = true;  // 1 byte
     
 }
 
@@ -26,7 +26,7 @@ class DataStoreTests: XCTestCase {
     
     struct Test : Init {
         
-        var a : Int
+        var a : Int  // 8 bytes
         
         init() {
             a = 0;
@@ -39,10 +39,13 @@ class DataStoreTests: XCTestCase {
     
     
     func testFile() -> NSURL {
-        let url = SOTools.tempDirectory()
+        //let url = SOTools.tempDirectory()
+        
+        let url = NSURL(fileURLWithPath : "/Users/steve/Test", isDirectory: true)
         return url.URLByAppendingPathComponent("data.db")
     }
     
+    /**
     func test0Delete() {
         var url = testFile()
         
@@ -50,22 +53,34 @@ class DataStoreTests: XCTestCase {
         
         XCTAssertFalse(url.isFileExisting(), "File not deleted?");
     }
+    */
     
     func test1StoreFile() {
         var url = testFile()
         
         NSLog("URL: %@", url.path!)
+        url.deleteFile()
         
         var dataStore = DataStore<TestHeader,Test>(url: url)
         
+        XCTAssertTrue(url.isFileExisting(),"A file exist?");
+        
         //XCTAssertNotNil(dataStore.fileHandle,"file is not created")
         XCTAssertNil(dataStore.error, "error happend?");
+        
+        //var dataValue = Test(num:42)
+        //var uid = dataStore.createBlock(dataValue)
         
         url.deleteFile()
         
         XCTAssertFalse(url.isFileExisting(),"File not deleted?");
     }
 
+    //
+    //  Create new DataStore
+    //  write a block
+    //  reopen this DataStore
+    //  read the block
     func test3WriteData() {
         
         var url = testFile()
@@ -91,9 +106,16 @@ class DataStoreTests: XCTestCase {
         
         result = dataStore.readBlock(uid);
         
-        XCTAssertEqual(result.a,44,"")
+        if result != nil {
+            XCTAssertEqual(result.a,42,"")
+        } else {
+            // result is nil
+            XCTFail()
+        }
+        
     }
-    
+
+
 }
 
 /**
