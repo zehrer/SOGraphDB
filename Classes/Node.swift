9c6d57df5fa26ac55b4eb9a6376d378985051cd8
Stephan Zehrer <stephan@zehrer.net>
2014-11-23 08:25:14 +0100

    moved old protocol part to model.swift put node to public removed UID (covered by GrapheElement)

--- Classes/Node.swift
+++ Classes/Node.swift
@@ -6,43 +6,6 @@
 //  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
 //
 
-import Foundation
-
-/**
-protocol Node {
-    
-    // OUT
-    var outRelationshipCount: Int? { get }
-    var firstOutNode: Node? { get }
-    var lastOutNode: Node? { get }
-    
-    var outRelationships: Relationship[] { get }
-    
-    func outRelationshipTo(node: Node) -> Relationship?
-    
-    func addOutRelationshipNode(node: Node) -> Relationship?
-    func deleteOutRelationshipNode(node: Node)
-    
-    //func outNodeEnumerator() -> NSEnumerator
-    
-    
-    // IN
-    var inRelationshipCount: Int? { get }
-    var firstInNode: Node? { get }
-    var lastInNode: Node? { get }
-    
-    var inRelationships: Relationship[] { get }
-    
-    func inRelationshipFrom(node: Node) -> Relationship?
-    
-    func addInRelationshipNode(node: Node) -> Relationship?
-    func deleteInRelationshipNode(node: Node)
-    
-    //func inNodeEnumerator() -> NSEnumerator
-    
-}
-*/
-
 struct NODE : Init {
     var nextPropertyID: UID = 0; // 4
     var nextOutRelationshipID: UID  = 0;  // 4
@@ -53,15 +16,10 @@ struct NODE : Init {
     }
 }
 
-
-
-class Node : GraphElement, Coding {
+public class Node : GraphElement, Coding {
 
     var data: NODE = NODE()
 
-    //var uid: UID? = nil
-    //var dirty: Bool = true
-    
     override required init() {
         
     }
@@ -71,8 +29,6 @@ class Node : GraphElement, Coding {
         self.data = data
         
     }
-    
-    
     
     // OUT
     var outRelationshipCount: Int? {
