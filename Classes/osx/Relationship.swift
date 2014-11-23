6fc3a93eff1235a6553fa02b324b1705a923cecf
Stephan Zehrer <stephan@zehrer.net>
2014-11-23 08:26:42 +0100

    moved protocol to model.swift started to migrated SORelationship to Swift

--- Classes/osx/Relationship.swift
+++ Classes/osx/Relationship.swift
@@ -6,10 +6,36 @@
 //  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
 //
 
-import Foundation
 
-protocol Relationship {
+struct RELATIONSHIP : Init {
+    var relationshipTypeNodeID: UID = 0; // 4
+    var nextPropertyID: UID  = 0;  // 4
     
-    init(startNode : Node)
+    var startNodeID: UID = 0; // 4
+    var startNodePrevRelationID: UID = 0; // 4
+    var startNodeNextRelationID: UID = 0; // 4
+    
+    var endNodeID: UID = 0; // 4
+    var endNodePrevRelationID: UID = 0; // 4
+    var endNodeNextRelationID: UID = 0; // 4
+    
+    init() {
+        
+    }
+}
+
+public class Relationship : GraphElement, Coding {
+    
+    var data: RELATIONSHIP = RELATIONSHIP()
+    
+    override required init() {
+        
+    }
+    
+    //decoding NSData
+    required init(data: NODE) {
+        self.data = data
+        
+    }
 
 }
