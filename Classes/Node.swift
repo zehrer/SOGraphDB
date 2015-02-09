d1b91bd8011ce726a18493f178d7e0f28db308e5
Stephan Zehrer <stephan@zehrer.net>
2015-02-09 08:40:17 +0100

    - set class to public - migrated several methods form SONode

--- Classes/Node.swift
+++ Classes/Node.swift
@@ -6,28 +6,33 @@
 //  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
 //
 
-struct NODE : Init {
+public struct NODE : Init {
     var nextPropertyID: UID = 0 // 4
     var nextOutRelationshipID: UID  = 0;  // 4
     var nextInRelationshipID: UID  = 0; // 4
     
-    init() {
+    public init() {
         
     }
 }
 
-class Node : GraphElement, Coding {
+public func == (lhs: Node, rhs: Node) -> Bool {
+    return lhs.uid == rhs.uid
+}
+
+public class Node : GraphElement, Coding, Equatable {
     
     // MARK: Coding
 
-    var data : NODE = NODE()
+    public var data : NODE = NODE()
+    
     
     // is required in the coding protocol
-    required init() {
+    required public init() {
     }
 
     //init with external value
-    required init(data: NODE) {
+    required public init(data: NODE) {
         //phase 1
         super.init()
         //phase 2
@@ -36,12 +41,51 @@ class Node : GraphElement, Coding {
     }
 
     // MARK: OUT
-    var outRelationshipCount: Int? {
-    get {
-        return 0;
-    }
+    
+    // only available for testing, not public
+    var outRelationshipID : UID {
+        get {
+            return data.nextOutRelationshipID
+        }
+        set {
+            if newValue != data.nextOutRelationshipID {
+                data.nextOutRelationshipID = newValue
+                dirty = true
+            }
+        }
     }
     
+    var _outRelationships:[Relationship]! = nil
+    
+    public var outRelationships: [Relationship] {
+        get {
+            if (_outRelationships == nil) {
+                _outRelationships = [Relationship]()
+                
+                // read data
+                var relationship:Relationship! = nil;
+                var nextRelationshipID = data.nextOutRelationshipID;
+                
+                while (nextRelationshipID > 0) {
+                    
+                    relationship = context.readRelationship(nextRelationshipID)
+                    
+                    _outRelationships.append(relationship)
+                    
+                    nextRelationshipID = relationship.startNodeNextRelationID
+                }
+            }
+            
+            return _outRelationships;
+        }
+    }
+    
+    var outRelationshipCount: Int {
+        get {
+            return outRelationships.count
+        }
+    }
+
     var firstOutNode: Node? {
     get {
         return nil;
@@ -54,24 +98,137 @@ class Node : GraphElement, Coding {
     }
     }
     
-    var outRelationships: [Relationship] {
-    get {
-        return [Relationship]()
-    }
-    }
-    
-    func outRelationshipTo(node: Node) -> Relationship? {
+    //find out relationship on disc
+    // TODO: this version is migrated from SONode
+    // improve the implementation e.g. reuse the informarion form outRelationships
+    func outRelationshipTo(endNode: Node) -> Relationship? {
+        // read data
+        var relationship:Relationship! = nil;
+        var nextRelationshipID = outRelationshipID
+        
+        while (nextRelationshipID > 0) {
+            
+            relationship = context.readRelationship(nextRelationshipID)
+            
+            if (relationship.endNodeID == endNode.uid) {
+                return relationship;
+            }
+            
+            nextRelationshipID = relationship.startNodeNextRelationID
+        }
+        
         return nil;
     }
     
-    func addOutRelationshipNode(node: Node) -> Relationship? {
+    // Create a new relation add it to the start node (this node) and the end node
+    // This methode update
+    //   - the new relationship
+    //   - (optional) the start node (itself) -> rel was appended directly
+    //   - (optional) the start node lastRelationship -> the rel was appended
+    //   - (optional) the end node - by calling insertInRelationship
+    //   - (optional) the end node lastRelationship - by calling insertInRelationship
+    func addOutRelationshipNode(endNode: Node) -> Relationship? {
+        
+        if (context != nil) {
+            // create an new realationship with a link to the this node
+            var relationship =  Relationship(startNode: self) //[[SORelationship alloc] initWithStartNode:self];
+            
+            // create the ID of this new relationship without a CONTEXT WRITE
+            // TODO: self registering??
+            context.registerRelationship(relationship)
+            
+            endNode.insertInRelationship(relationship)
+            
+            //NSMutableArray *outRelationships = [self outRelationshipArray];
+            var outRelationships = self.outRelationships
+            
+            //SORelationship *lastRelationship = [outRelationships lastObject];
+            var lastRelationship = outRelationships.last
+            
+            // TODO: optional chaining?
+            if (lastRelationship != nil) {
+                // it seems this node has already one or more relationships
+                // add relationship to the last one
+                relationship.startNodePreviousRelationID = lastRelationship!.uid
+                lastRelationship!.startNodeNextRelationID = relationship.uid
+                
+                // updated of the LAST relationship is only required if
+                // the is was extended
+                // CONTEXT WRITE
+                //[[self context] updateRelationship:lastRelationship];
+                context.updateRelationship(lastRelationship!)
+                
+            } else {
+                // it seems this is the frist relationship
+                // add relationship to the node
+                //[self setOutRelationshipID:relationship.id];
+                self.outRelationshipID = relationship.uid
+                
+                // CONTEXT WRITE
+                // update of self is only required if the id was set
+                self.update()
+            }
+            
+            // CONTEXT WRITE
+            context.updateRelationship(relationship)
+            
+            //[outRelationships addObject:relationship];
+            outRelationships.append(relationship)
+            
+            return relationship;
+        }
+        
         return nil;
     }
     
-    func deleteOutRelationshipNode(node: Node) {
+    // Delete a existing relationship between this node (start node) and the specified node (end node)
+    func deleteOutRelationshipNode(endNode: Node) {
+        
+        var relationship = self.outRelationshipTo(endNode)
+        if (relationship != nil) {
+            relationship!.delete()
+        }
         
     }
     
+    func deleteOutRelationship(aRelationship:Relationship) {
+        var previousRelationship:Relationship! = nil
+        var nextRelationship:Relationship! = nil
+        
+        var nextRelationshipID = aRelationship.startNodeNextRelationID
+        var previousRelationshipID = aRelationship.startNodePreviousRelationID
+        
+        if (nextRelationshipID > 0) {
+            nextRelationship = context.readRelationship(nextRelationshipID)
+            
+            nextRelationship.startNodePreviousRelationID = previousRelationshipID
+            
+            // CONTEXT WRITE
+            context.updateRelationship(nextRelationship)
+        }
+        
+        if (previousRelationshipID > 0) {
+            previousRelationship = context.readRelationship(previousRelationshipID)
+            
+            previousRelationship.startNodeNextRelationID = nextRelationshipID
+            
+            // CONTEXT WRITE
+            context.updateRelationship(previousRelationship)
+            
+        } else {
+            // seems this is the first relationship in the chain
+            outRelationshipID = nextRelationshipID
+            
+            // CONTEXT WRITE
+            // update of self is only required if the id was set
+            self.update()
+        }
+        
+        var index = find(outRelationships, aRelationship) // init outRelationships in worst case
+        _outRelationships.removeAtIndex(index!)
+    }
+
+
     /*
     func outNodeEnumerator() -> NSEnumerator {
     return SONodeEnumerator(
@@ -80,28 +237,51 @@ class Node : GraphElement, Coding {
     
     
     // MARK: IN
-    var inRelationshipCount: Int? {
-    get {
-        return 0;
+    
+    // only available for testing, not public
+    var inRelationshipID : UID {
+        get {
+            return data.nextInRelationshipID
+        }
+        set {
+            if newValue != data.nextInRelationshipID {
+                data.nextInRelationshipID = newValue
+                dirty = true
+            }
+        }
     }
+    
+    var _inRelationships:[Relationship]! = nil
+    
+    public var inRelationships: [Relationship] {
+        get {
+            if (_inRelationships == nil) {
+                _inRelationships = [Relationship]()
+                
+// TODO
+            }
+            
+            return _inRelationships;
+        }
+    }
+
+    
+    var inRelationshipCount: Int? {
+        get {
+            return 0;
+        }
     }
     
     var firstInNode: Node? {
-    get {
-        return nil;
-    }
+        get {
+            return nil;
+        }
     }
     
     var lastInNode: Node? {
-    get {
-        return nil;
-    }
-    }
-    
-    var inRelationships: [Relationship] {
-    get {
-        return [Relationship]()
-    }
+        get {
+            return nil;
+        }
     }
     
     func inRelationshipFrom(node: Node) -> Relationship? {
@@ -116,9 +296,80 @@ class Node : GraphElement, Coding {
         
     }
     
-    //func inNodeEnumerator() -> NSEnumerator
-    
+    // Update the ID's the new specified IN relationship
+    // This method update
+    //  - optional : the end node (itself) -> rel was appended directly
+    //  - optional : the lastRelationship if the rel was appended
+    func insertInRelationship(relationship: Relationship) {
+        // called by addOutRelationship
+    }
 
+    func deleteInRelationship(aRelationship:Relationship) {
+        var previousRelationship : Relationship! = nil;
+        var nextRelationship : Relationship! = nil;
+        
+        var nextRelationshipID = aRelationship.endNodeNextRelationID
+        var previousRelationshipID = aRelationship.endNodePreviousRelationID
+        
+        if (nextRelationshipID > 0) {
+            nextRelationship = context.readRelationship(nextRelationshipID)
+            
+            nextRelationship.endNodePreviousRelationID = previousRelationshipID
+            
+            // CONTEXT WRITE
+            context.updateRelationship(nextRelationship)
+        }
+        
+        if (previousRelationshipID > 0 ) {
+            previousRelationship = context.readRelationship(previousRelationshipID)
+            
+            previousRelationship.endNodeNextRelationID = nextRelationshipID
+            
+            // CONTEXT WRITE
+            context.updateRelationship(previousRelationship)
+            
+        } else {
+            // seems this is the first relationship in the chain
+            self.inRelationshipID = nextRelationshipID
+            
+            // CONTEXT WRITE
+            // update of self is only required if the id was set
+            self.update()
+        }
+        
+        var index = find(inRelationships, aRelationship) // init outRelationships in worst case
+        _inRelationships.removeAtIndex(index!)
+        
+    }
+    
+    //func inNodeEnumerator() -> NSEnumerator
+    //  return [[SONodeEnumerator alloc] initWithNode:self];
+
+    // MARK: Related (OUT) Nodes
+    
+    func relatedNodes() -> [Node] {
+        
+        var result = [Node]()
+        
+        if (context != nil) {
+            // UseCase: new (in context) -> isDiry = false && context != nil  (if the context store directly)
+            // Usecase: updated (in context) -> isDiry =
+            // TODO: updated this text
+            
+            let outArray = outRelationships
+            
+            for relationship in outArray {
+                var aNode = context.readNode(relationship.endNodeID)
+                if (aNode != nil) {
+                    result.append(aNode!)
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    
     // MARK: ??
     
     func update() {
@@ -126,17 +377,13 @@ class Node : GraphElement, Coding {
     }
     
     // MARK: Property
-    var propertyID : UID? {
+    var propertyID : UID {
         get {
             return data.nextPropertyID
         }
-        set(newID) {
-            if newID != data.nextPropertyID {
-                if (newID != nil) {
-                    data.nextPropertyID = newID!
-                } else {
-                    data.nextPropertyID = 0
-                }
+        set {
+            if newValue != data.nextPropertyID {
+                data.nextPropertyID = newValue
                 dirty = true
             }
         }
