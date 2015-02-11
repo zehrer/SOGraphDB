98345a163f5b843f00e3094266ff999c54644f63
Stephan Zehrer <stephan@zehrer.net>
2015-02-11 07:29:10 +0100

    added new init and several properties

--- Classes/Property.swift
+++ Classes/Property.swift
@@ -30,6 +30,7 @@ public struct PROPERTY : Init {
     var isNodeSource: Bool = false;         // 1 Byte  <- yes = property of a node / no = property of a relationship
     var isUTF8Encoding: Bool = true;        // 1 Byte  <- yes internal string usues UTF8 / NO == UTF16
     
+    // TODO use default length?
     var bufferLength: UInt8 = 0;            // 1 Byte
     
     var type: PropertyType = .kUndefined;   // 1 Byte
@@ -66,6 +67,62 @@ public class Property : GraphElement, Coding, Equatable {
         //phase 2
         dirty = false
     }
+    
+    required public init(graphElement:PropertyAccessElement) {
+        //phase 1
+        super.init()
+        //phase 2
+        
+        data.sourceID = graphElement.uid
+        
+        // default is false
+        if graphElement is Node {
+            data.isNodeSource = true;
+        }
+        
+        //data.bufferLength = BUFFER_LEN;
+    }
+    
+
+    // only available for testing, not public
+    var previousPropertyID : UID {
+        get {
+            return data.prevPropertyID
+        }
+        set {
+            if newValue != data.prevPropertyID {
+                data.prevPropertyID = newValue
+                dirty = true
+            }
+        }
+    }
+    
+    // only available for testing, not public
+    var nextPropertyID : UID {
+        get {
+            return data.nextPropertyID
+        }
+        set {
+            if newValue != data.nextPropertyID {
+                data.nextPropertyID = newValue
+                dirty = true
+            }
+        }
+    }
+    
+    // only available for testing, not public
+    var keyNodeID : UID {
+        get {
+            return data.propertyKeyNodeID
+        }
+        set {
+            if newValue != data.propertyKeyNodeID {
+                data.propertyKeyNodeID = newValue
+                dirty = true
+            }
+        }
+    }
+    
 
 }
 
