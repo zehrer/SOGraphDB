3ad0171ad50fa4ea6e3f1bb266d494f662849525
Stephan Zehrer <stephan@zehrer.net>
2015-04-12 21:53:10 +0200

    Copy of original ObjectStore which was renamed to ObjectDataStore

---
+++ Tests/ObjectDataStoreTestx.swift
@@ -0,0 +1,118 @@
+//
+//  ObjectStoreTests.swift
+//  SOGraphDB
+//
+//  Created by Stephan Zehrer on 27.08.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+
+import XCTest
+import SOGraphDB
+
+internal class TestDataClass : Coding {
+    
+    struct TestData : Init {
+        
+        var a : Int  // 8 bytes
+        
+        init() {
+            a = 0;
+        }
+        
+        init(num: Int) {
+            a = num
+        }
+    }
+    
+    var data: TestData = TestData()
+    
+    var uid: UID!
+    var dirty: Bool = true
+    
+    var num : Int {
+        get {
+            return self.data.a;
+        }
+    }
+    
+    required init() {
+        // data = TestData()
+    }
+    
+    init (num : Int) {
+        // data = TestData()
+        self.data.a = num
+    }
+    
+    //decoding
+    required init(data: TestData) {
+        self.data = data
+    }
+}
+
+class ObjectDataStoreTests: XCTestCase {
+    
+    func testFile() -> NSURL {
+        //let url = SOTools.tempDirectory()
+        
+        let url = NSURL(fileURLWithPath : "/Users/steve/Test", isDirectory: true)
+        return url!.URLByAppendingPathComponent("object.db")
+    }
+    
+    func test1StoreFile() {
+        var url = testFile()
+        
+        NSLog("URL: %@", url.path!)
+        url.deleteFile()
+        
+        var objectStore = ObjectDataStore<TestDataClass>(url: url)
+        
+        XCTAssertTrue(url.isFileExisting(),"A file exist?");
+        
+        //XCTAssertNotNil(dataStore.fileHandle,"file is not created")
+        XCTAssertNil(objectStore.error, "error happend?");
+        
+        url.deleteFile()
+        
+        XCTAssertFalse(url.isFileExisting(),"File not deleted?");
+    }
+    
+    //
+    //  Create new DataStore
+    //  write a block
+    //  reopen this DataStore
+    //  read the block
+    func test2WriteData() {
+        
+        var url = testFile()
+        url.deleteFile()
+        
+        var objectStore = ObjectDataStore<TestDataClass>(url: url)
+        
+        //XCTAssertNotNil(dataStore.fileHandle,"file is not created");
+        XCTAssertNil(objectStore.error, "error happend?");
+        
+        var dataClass = TestDataClass(num:42)
+        
+        var uid = objectStore.addObject(dataClass)
+        
+        XCTAssertEqual(uid, 1, "")
+        
+        //ObjectDataStore = nil
+        objectStore = ObjectDataStore<TestDataClass>(url: url)
+        
+        //XCTAssertNotNil(dataStore.fileHandle,@"file is not created");
+        //XCTAssertNil(ObjectDataStore.error, "error happend?");
+        
+        var result = objectStore.readObject(uid);
+        
+        if result != nil {
+            XCTAssertEqual(result.num,42,"")
+        } else {
+            // result is nil
+            XCTFail()
+        }
+        
+    }
+}
