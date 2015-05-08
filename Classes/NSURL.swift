811da6aec5dac82ee29ddc3c4c402de7e0423566
Stephan Zehrer <stephan@zehrer.net>
2015-05-08 10:37:52 +0200

    Initial Version migrated from ObjC

---
+++ Classes/NSURL.swift
@@ -0,0 +1,56 @@
+//
+//  NSURL.swift
+//  SOGraphDB
+//
+//  Created by Stephan Zehrer on 08.05.15.
+//  Copyright (c) 2015 Stephan Zehrer. All rights reserved.
+//
+
+import Foundation
+
+
+extension NSURL {
+   
+    func isFileExisting() -> Bool {
+        let path = self.path
+        
+        if path != nil {
+            return NSFileManager.defaultManager().fileExistsAtPath(path!)
+        } else {
+          return false
+        }
+    }
+    
+    func deleteFile() {
+        if self.isFileExisting() {
+            var error : NSError? = nil
+            NSFileManager.defaultManager().removeItemAtURL(self, error: &error)
+            
+            if error != nil {
+                NSLog("ERROR: during delet of file: \(path)")
+            }
+        }
+    }
+}
+
+/**
+- (BOOL)isFileExisting;
+{
+    return [[NSFileManager defaultManager] fileExistsAtPath:[self path]];
+}
+
+- (void)deleteFile;
+{
+    if ([self isFileExisting]) {
+        NSError *aError = nil;
+        
+        [[NSFileManager defaultManager] removeItemAtURL:self error:&aError];
+        
+        if (aError) {
+            NSLog(@"ERROR: delete %@",[self path]);
+        }
+    }
+}
+
+
+*/
