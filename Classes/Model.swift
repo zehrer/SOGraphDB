f8f2924aba99b1a8fa419c9931cfa4e5f0f2dd8f
Stephan Zehrer <stephan@zehrer.net>
2015-06-20 23:29:30 +0200

    moved to classes folder

---
+++ Classes/Model.swift
@@ -0,0 +1,59 @@
+//
+//  Model.swift
+//  SOGraphDB
+//
+//  Created by Stephan Zehrer on 23.11.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+import Foundation
+
+
+/**
+protocol Node {
+    
+    // OUT
+    var outRelationshipCount: Int? { get }
+    var firstOutNode: Node? { get }
+    var lastOutNode: Node? { get }
+    
+    var outRelationships: Relationship[] { get }
+    
+    func outRelationshipTo(node: Node) -> Relationship?
+    
+    func addOutRelationshipNode(node: Node) -> Relationship?
+    func deleteOutRelationshipNode(node: Node)
+    
+    //func outNodeEnumerator() -> NSEnumerator
+    
+    
+    // IN
+    var inRelationshipCount: Int? { get }
+    var firstInNode: Node? { get }
+    var lastInNode: Node? { get }
+    
+    var inRelationships: Relationship[] { get }
+    
+    func inRelationshipFrom(node: Node) -> Relationship?
+    
+    func addInRelationshipNode(node: Node) -> Relationship?
+    func deleteInRelationshipNode(node: Node)
+    
+    //func inNodeEnumerator() -> NSEnumerator
+    
+}
+*/
+
+/**
+protocol Relationship {
+
+init(startNode : Node)
+
+}
+
+
+protocol Property {
+
+
+}
+*/
