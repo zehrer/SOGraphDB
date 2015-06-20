f7ff8c7643f7ee27ca92de703d14afcd58cfe7dc
Stephan Zehrer <stephan@zehrer.net>
2015-06-20 23:15:59 +0200

    moved in classes folder

---
+++ Classes/GraphElement.swift
@@ -0,0 +1,33 @@
+//
+//  GraphElement.swift
+//  SOGraphDB
+//
+//  Created by Stephan Zehrer on 16.06.14.
+//  Copyright (c) 2014 Stephan Zehrer. All rights reserved.
+//
+
+import Foundation
+
+
+public class GraphElement : NSObject {
+    
+    // TODO: ! -> ? (to verify more bugs)
+    public var uid: UID? = nil
+    public var dirty: Bool = true
+    
+    // TODO: is public required?
+    public weak var context : GraphContext? = nil
+  
+    required public override init() {
+        
+    }
+    
+/**
+
+    //#pragma mark - SOCoding
+    
+    func update() {
+        NSLog("ERROR: override this methode");
+    }
+    */
+}
