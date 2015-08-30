8c2aa5d2b800ec7196277507fa55b8be3cbc2ce9
Stephan Zehrer <stephan@zehrer.net>
2015-08-30 06:56:50 +0200

    removed didSet dirty trigger ... will be replaced later on by a timestamp

--- Swift/Property.swift
+++ Swift/Property.swift
@@ -41,37 +41,12 @@ public struct Property : ValueStoreElement, Context {
     
     var isNodeSource: Bool = false;         // 3  <- yes = property of a node / no = property of a relationship
     
-    var sourceID: UID = 0 {              //  <- link to the source object
-        didSet {
-            if sourceID != oldValue {
-                dirty = true
-            }
-        }
-    }
-    
-    var keyNodeID: UID = 0 {     //  <- "type" of this property
-        didSet {
-            if keyNodeID != oldValue {
-                dirty = true
-            }
-        }
-    }
+    var sourceID: UID = 0  // the ID of the related node or relationship
+    var keyNodeID: UID = 0
 
-    var previousPropertyID: UID = 0 {            //  !<- 0 if start
-        didSet {
-            if previousPropertyID != oldValue {
-                dirty = true
-            }
-        }
-    }
-    
-    var nextPropertyID: UID = 0 {          //  !<- 0 if end
-        didSet {
-            if nextPropertyID != oldValue {
-                dirty = true
-            }
-        }
-    }
+    var previousPropertyID: UID = 0
+    var nextPropertyID: UID = 0
+
     
     public var type: PropertyType = .Nil;
 
