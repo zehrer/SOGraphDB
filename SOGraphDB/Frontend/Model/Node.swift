5a466eacbed3ae066a8bb7b6bfeecf6fb1f7ffc1
Stephan Zehrer <stephan@zehrer.net>
2017-12-05 19:24:07 +0100

    updated addOutRelationshipTo added insert(outRelationship ) added addInRelationshipFrom updated insert(inRelationship)

--- SOGraphDB/Frontend/Model/Node.swift
+++ SOGraphDB/Frontend/Model/Node.swift
@@ -57,84 +57,54 @@ public class Node : PropertyAccessElement { //Equatable
     
     // Create a new relation add it to the start node (this node) and the end node
     // This methode update
-    //   - the new relationship
-    //   - (optional) the start node (itself) -> rel was appended directly
-    //   - (optional) the start node lastRelationship -> the rel was appended
-    //   - (optional) the end node - by calling insertInRelationship
-    //   - (optional) the end node lastRelationship - by calling insertInRelationship
-    // Why return optional?
-    public func addOutRelationshipNode(endNode: Node) -> Relationship? {
+    //   - create and register a new relationship
+    //   - add the relationship to the itself (call insert)
+    //   - add the relationship to the end node (call insert)
+    // TODO: the old version the return value was optional, why?
+    public func addOutRelationshipTo(endNode: Node) -> Relationship {
         assert(graphStore != nil, "No GrapheStore available")
         
         let relationship =  Relationship(startNode: self, endNode: endNode)
         // TODO: implement self register
-        
         graphStore.register(relationship: relationship)
         
         endNode.insert(inRelationship: relationship)
-        
-        self._outRelationships.append(relationship)
-        
-        
-        /**
-        if (context != nil) {
-            // create an new realationship with a link to the this node
-            let relationship =  Relationship(startNode: self) //[[SORelationship alloc] initWithStartNode:self];
-            
-            // create the ID of this new relationship without a CONTEXT WRITE
-            // TODO: self registering??
-            context!.registerRelationship(relationship)
-            
-            endNode.insertInRelationship(relationship)
-            
-            //NSMutableArray *outRelationships = [self outRelationshipArray];
-            let outRelationships = self.outRelationships
-            
-            //SORelationship *lastRelationship = [outRelationships lastObject];
-            let lastRelationship = outRelationships.last
-            
-            // TODO: optional chaining?
-            if (lastRelationship != nil) {
-                // it seems this node has already one or more relationships
-                // add relationship to the last one
-                relationship.startNodePreviousRelationID = lastRelationship!.uid!
-                lastRelationship!.startNodeNextRelationID = relationship.uid!
-                
-                // updated of the LAST relationship is only required if
-                // the is was extended
-                // CONTEXT WRITE
-                //[[self context] updateRelationship:lastRelationship];
-                context!.updateRelationship(lastRelationship!)
-                
-            } else {
-                // it seems this is the frist relationship
-                // add relationship to the node
-                //[self setOutRelationshipID:relationship.id];
-                self.outRelationshipID = relationship.uid!
-                
-                // CONTEXT WRITE
-                // update of self is only required if the id was set
-                self.update()
-            }
-            
-            // CONTEXT WRITE
-            context!.updateRelationship(relationship)
-            
-            //[outRelationships addObject:relationship];
-            _outRelationships.append(relationship)
-            
-            return relationship;
-        }
- 
-        */
+        self.insert(outRelationship: relationship)
         
         return relationship
     }
     
+    // Update the outRelationship and notify the graphStore
+    func insert(outRelationship aRel: Relationship) {
+        assert(graphStore != nil, "No GrapheStore available")
+        
+        _outRelationships.append(aRel)
+        // TODO: improve details
+        graphStore.update(node: self)
+    }
+    
     // MARK: IN
     
-    // Update the _inRelationship
-    // INFO: e.g. called by addOutRelationship
+    // Create a new relation add it to the start node and the end node (this node)
+    // This methode update
+    //   - create and register a new relationship
+    //   - add the relationship to the start node (call insert)
+    //   - add the relationship to the itself (call insert)
+    public func addInRelationshipFrom(startNode: Node) -> Relationship {
+        assert(graphStore != nil, "No GrapheStore available")
+        
+        let relationship =  Relationship(startNode: startNode, endNode: self)
+        // TODO: implement self register
+        graphStore.register(relationship: relationship)
+        
+        self.insert(inRelationship: relationship)
+        startNode.insert(outRelationship: relationship)
+        
+        return relationship
+    }
+    
+    
+    // Update the inRelationship and notify the graphStore
     func insert(inRelationship aRel: Relationship) {
         assert(graphStore != nil, "No GrapheStore available")
         
