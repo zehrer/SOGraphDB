f48bee651c022eb4d18df4e7b7c2d52919c6cde1
Stephan Zehrer <stephan@zehrer.net>
2017-12-26 21:30:23 +0100

    Initial Version?

---
+++ SOGraphDB/Frontend/Context/SOGrapheDB.swift
@@ -0,0 +1,51 @@
+//
+//  SOGrapheDB.swift
+//  SOGraphDB-Mac
+//
+//  Created by Stephan Zehrer on 13.11.17.
+//  Copyright © 2017 Stephan Zehrer. All rights reserved.
+//
+//  Feature List:
+//  - Auto Register (TODO)
+//
+//
+
+
+
+import Foundation
+
+open class SOGrapheDB {
+    
+    private let graphDBStore : SOGraphDBStore
+    
+    //MARK:  -
+    
+    public init(aStore : SOGraphDBStore) {
+        self.graphDBStore = aStore
+    }
+    
+    
+    /**
+     No create -> later feature: auto register
+    // create (and register) a new Node
+    open func createNode() -> Node {
+        let result = Node()
+        register(node: result)
+        return result;
+    }
+    */
+ 
+    // register a (new created) Node to DB
+    open func register(node aNode : Node) {
+        aNode.graphStore = self.graphDBStore
+        graphDBStore.register(aNode)
+    }
+    
+    // delete/remove a (registered) Node from the DB
+    open func delete(node aNode: Node) {
+        graphDBStore.delete(aNode)
+        aNode.graphStore = nil;
+    }
+    
+}
+    
