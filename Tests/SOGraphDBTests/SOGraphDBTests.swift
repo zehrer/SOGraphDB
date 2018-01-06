661939a3c4d1b51a96128f766397531763cfcb00
Stephan Zehrer <stephan@zehrer.net>
2018-01-06 15:44:06 +0100

    disabled the empty testes

--- Tests/SOGraphDBTests/SOGraphDBTests.swift
+++ Tests/SOGraphDBTests/SOGraphDBTests.swift
@@ -2,15 +2,20 @@ import XCTest
 @testable import SOGraphDB
 
 class SOGraphDBTests: XCTestCase {
+    
+    /**
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         //XCTAssertEqual(SOGraphDB().text, "Hello, World!")
     }
+ 
 
 
     static var allTests = [
         ("testExample", testExample),
     ]
+ 
+     */ 
 }
