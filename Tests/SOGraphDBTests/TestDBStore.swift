ae35dd928d42d28491b0e663ca5b2f962007f5cf
Stephan Zehrer <stephan@zehrer.net>
2018-02-16 15:14:21 +0100

    initial version

---
+++ Tests/SOGraphDBTests/TestDBStore.swift
@@ -0,0 +1,49 @@
+//
+//  TestDBStore.swift
+//  SOGraphDBTests
+//
+//  Created by Stephan Zehrer on 16.02.18.
+//
+
+import Foundation
+import SOGraphDB
+
+public class TestDBStore : SOGraphDBStore {
+    
+    public required init()  {
+    }
+    
+    public func register(_ aNode: Node) -> Node? {
+        return nil
+    }
+    
+    public func findNodeBy(uid: UID?) -> Node? {
+        return nil
+    }
+    
+    public func update(_ aNode: Node) {
+        
+    }
+    
+    public func delete(_ aNode: Node) {
+        
+    }
+    
+    public func register(_ aRelationship: Relationship) {
+        
+    }
+    
+    public func update(_ aRelationship: Relationship) {
+        
+    }
+    
+    public func delete(_ aRelationship: Relationship) {
+        
+    }
+    
+    public func findRelationship(from startNode: Node, to endNode: Node) -> Relationship? {
+        return nil
+    }
+
+}
+
